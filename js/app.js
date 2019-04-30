14a0e8b56e9a984fb35a019242140064a9fb1af9
Juan <jberrioscifuentes@gmail.com>
2019-04-30 16:34:45 -0400

    =

--- js/app.js
+++ js/app.js
@@ -1,56 +1,16 @@
 $(function(){
 
   // Scroll -> Link To
-  $('#link-1').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-  });
-
-  // Animations Scroller
-  $(window).scroll(function() {
-      // var scroll = $(window).scrollTop();
-      // var preFeatures = $('.hero').offset().top + 200;
-      // var preAbout = $('.features-inner').offset().top - 200;
-      // var preNews = $('.about').offset().top - 200;
-      // var preOpaline = $('.news').offset().top - 200;
-      // var preTeam = $('.opaline').offset().top - 200;
-      // var preGallery = $('.team').offset().top - 200;
-      // // var preSponsors = $('.gallery').offset().top + 400;
-      // var prePreFooter = $('.gallery').offset().top + 400;
-      //
-      // if(scroll > preFeatures){
-      //     $('.features-inner').addClass('fadeIn');
-      // }
-      // if(scroll > preAbout){
-      //   $('.about-content-inner').addClass('fadeIn');
-      // }
-      // if(scroll > preNews){
-      //   $('.news-inner').addClass('fadeIn');
-      // }
-      // if(scroll > preOpaline){
-      //   $('.opaline-content-inner').addClass('fadeIn');
-      // }
-      // if(scroll > preTeam){
-      //   $('.team-inner').addClass('fadeIn');
-      // }
-      // if(scroll > preGallery){
-      //   $('.gallery-inner').addClass('fadeIn');
-      // }
-      // // if(scroll > preSponsors){
-      // //   $('.sponsors-inner').addClass('fadeIn');
-      // // }
-      // if(scroll > prePreFooter){
-      //   $('.prefooter-inner .row').addClass('fadeIn');
-      // }
-
-  });
+  // $('#link-1').click(function(){
+  //   $('html, body').animate({
+  //       scrollTop: $( $.attr(this, 'href') ).offset().top
+  //   }, 500);
+  //   return false;
+  // });
 
   new WOW().init();
 
   $('.gallery').masonry({
-    // options
     itemSelector : '.item'
   });
 
