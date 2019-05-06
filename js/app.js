dbf3007bf3f79c78dbe933bdcc9a6bd78e16f898
Juan <jberrioscifuentes@gmail.com>
2019-05-06 17:10:01 -0400

    ===

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
 $(function(){
 
   function whenIsLoaded(){
-    $('.loading').addClass('fadeOut');
     setTimeout(function(){
-      $('header, .hero-inner h1, .hero-inner h5, .hero-inner a').removeClass('d-none');
-      $('body').removeClass('overflow-block');
+      $('.loading').addClass('fadeOut');
+      setTimeout(function(){
+        $('header, .hero-inner h1, .hero-inner h5, .hero-inner a').removeClass('d-none');
+        $('body').removeClass('overflow-block');
+      }, 1000);
     }, 1000);
 
     new WOW().init();
 
-    // $('.gallery').masonry({
-    //   itemSelector : '.item'
-    // });
+    function Parallax() {
+    	scrollPos = $(this).scrollTop();
+
+    	$('.hero').css({
+    		'background-position': '50% ' + (30-scrollPos/10)+'%'
+    	});
+    }
+
+    $(window).scroll(function() {
+  		Parallax();
+  	});
   }
 
   window.onload = whenIsLoaded();
