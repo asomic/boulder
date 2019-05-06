48311f2adfbd0ed96364ee3955626823382ffedf
Juan <jberrioscifuentes@gmail.com>
2019-05-06 17:40:22 -0400

    RR.SS

--- js/app.js
+++ js/app.js
@@ -5,7 +5,10 @@ $(function(){
       $('.loading').addClass('fadeOut');
       setTimeout(function(){
         $('header, .hero-inner h1, .hero-inner h5, .hero-inner a').removeClass('d-none');
-        $('body').removeClass('overflow-block');
+        // $('body').removeClass('overflow-block');
+        $('body').css({
+          'overflow': 'auto'
+        });
       }, 1000);
     }, 1000);
 
